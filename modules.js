9d6b8352280e01a90436e09a56b887d32726d746
Lukas <hungduonggia181@gmail.com>
2021-07-13 15:31:27 +0700

    first commit

---
+++ modules.js
@@ -0,0 +1,9 @@
+const names = require('./names')
+const sayHi = require('./utils')
+const extra_module = require('./extra')
+require('./mind-grenade')
+
+
+sayHi('Sussan')
+sayHi(names.lion)
+sayHi(names.hung)
---
+++ names.js
@@ -0,0 +1,8 @@
+const lion = 'lion'
+const hung = 'hung'
+const secret = 'SUPER SECRET'
+
+module.exports = {
+    lion,
+    hung
+}
